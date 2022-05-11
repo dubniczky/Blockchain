cc9b5e7829f018e883c1bf2d883045dafbf75a39
Richard Antal Nagy <nagy.richard.antal@gmail.com>
2022-05-12 01:23:00 +0200

    Added chain testing to block validity

--- test/block_test.py
+++ test/block_test.py
@@ -16,10 +16,10 @@ def test_block_previous():
 def test_block_mine():
     complexity = 2
     b1 = Block("", {'var': 'val'}, complexity, time=datetime(1997, 11, 27, 11, 32, 23, 19233))
+    b1.mine()
     b2 = Block(b1.hash, {'item': 13_556}, complexity, time=datetime(1993, 3, 12, 10, 0, 3, 234))
-    b3 = Block(b2.hash, {}, complexity, time=datetime(2000, 1, 1, 0, 0, 0, 3234))
-
     b2.mine()
+    b3 = Block(b2.hash, {}, complexity, time=datetime(2000, 1, 1, 0, 0, 0, 3234))
     b3.mine()
 
     assert b2.hash.startswith('0' * complexity)
@@ -28,15 +28,18 @@ def test_block_mine():
 def test_block_validity():
     complexity = 2
     b1 = Block("", {'var': 'val'}, complexity, time=datetime(1997, 11, 27, 11, 32, 23, 19233))
+    b1.mine()
     b2 = Block(b1.hash, {'item': 13_556}, complexity, time=datetime(1993, 3, 12, 10, 0, 3, 234))
-    b3 = Block(b2.hash, {}, complexity, time=datetime(2000, 1, 1, 0, 0, 0, 3234))
-
     b2.mine()
+    b3 = Block(b2.hash, {}, complexity, time=datetime(2000, 1, 1, 0, 0, 0, 3234))
     b3.mine()
 
     assert b1.valid()
     assert b2.valid()
     assert b3.valid()
+
+    assert b1.hash == b2.previous_hash
+    assert b2.hash == b3.previous_hash
 
     b2.data = {'item': 13_555}
 
