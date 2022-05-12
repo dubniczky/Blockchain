dca4b8b0f778658dba691f64038c7c922c724233
Richard Antal Nagy <nagy.richard.antal@gmail.com>
2022-05-12 02:44:43 +0200

    Moved mined block creation to fixture in block test

--- test/block_test.py
+++ test/block_test.py
@@ -1,5 +1,19 @@
+import pytest
 from datetime import datetime
 from blockchain import Block
+
+complexity = 2
+
+@pytest.fixture(scope='module')
+def mined_blocks():
+    global complexity
+    b1 = Block("", {'var': 'val'}, complexity, time=datetime(1997, 11, 27, 11, 32, 23, 19233))
+    b1.mine()
+    b2 = Block(b1.hash, {'item': 13_556}, complexity, time=datetime(1993, 3, 12, 10, 0, 3, 234))
+    b2.mine()
+    b3 = Block(b2.hash, {}, complexity, time=datetime(2000, 1, 1, 0, 0, 0, 3234))
+    b3.mine()
+    return [b1, b2, b3]
 
 def test_block_empty():
     b = Block("", None, 2)
@@ -13,26 +27,14 @@ def test_block_previous():
 
     assert b1.hash == b2.previous_hash
 
-def test_block_mine():
-    complexity = 2
-    b1 = Block("", {'var': 'val'}, complexity, time=datetime(1997, 11, 27, 11, 32, 23, 19233))
-    b1.mine()
-    b2 = Block(b1.hash, {'item': 13_556}, complexity, time=datetime(1993, 3, 12, 10, 0, 3, 234))
-    b2.mine()
-    b3 = Block(b2.hash, {}, complexity, time=datetime(2000, 1, 1, 0, 0, 0, 3234))
-    b3.mine()
+def test_block_mine(mined_blocks):
+    global complexity
 
-    assert b2.hash.startswith('0' * complexity)
-    assert b3.hash.startswith('0' * complexity)
+    assert mined_blocks[1].hash.startswith('0' * complexity)
+    assert mined_blocks[2].hash.startswith('0' * complexity)
 
-def test_block_validity():
-    complexity = 2
-    b1 = Block("", {'var': 'val'}, complexity, time=datetime(1997, 11, 27, 11, 32, 23, 19233))
-    b1.mine()
-    b2 = Block(b1.hash, {'item': 13_556}, complexity, time=datetime(1993, 3, 12, 10, 0, 3, 234))
-    b2.mine()
-    b3 = Block(b2.hash, {}, complexity, time=datetime(2000, 1, 1, 0, 0, 0, 3234))
-    b3.mine()
+def test_block_validity(mined_blocks):
+    b1, b2, b3 = mined_blocks
 
     assert b1.valid()
     assert b2.valid()
